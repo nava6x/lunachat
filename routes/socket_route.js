8ee7db9747cf9fe6e22ff5b9a7be3153ad5aea5a
nava6x <navafacts2026@gmail.com>
2024-12-17 02:31:29 +0530

    Update socket_route.js

--- routes/socket_route.js
+++ routes/socket_route.js
@@ -1,7 +1,7 @@
 const socketIo = require('socket.io');
 // require('dotenv').config();
 
-const frontendUrl = process.env.FRONT_END || 'https://6760688a222b23305dc967be--golden-douhua-9cb439.netlify.app'; 
+const frontendUrl = 'https://676093f28996fd834239eb39--golden-douhua-9cb439.netlify.app/'; 
 
 module.exports = (server) => {
   const io = socketIo(server, {
